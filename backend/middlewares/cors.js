bef476d8827ddc7656df35d1c3b97c1b0907b06e
Shevchenko Ekaterina <shev4enko.ekat@yandex.ru>
2023-02-05 16:11:25 +0300

    15пр1

---
+++ backend/middlewares/cors.js
@@ -0,0 +1,22 @@
+const app = express();
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+];
+
+const { method } = req;
+
+const requestHeaders = req.headers['access-control-request-headers'];
+if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+}
+
+app.use(function(req, res, next) {
+  const { origin } = req.headers;
+    if (allowedCors.includes(origin)) {
+      res.header('Access-Control-Allow-Origin', origin);
+    }
+  next();
+});
